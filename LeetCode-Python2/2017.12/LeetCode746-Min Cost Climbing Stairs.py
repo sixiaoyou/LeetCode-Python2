e38126525070de089ada1c27f7080ec2b57b83bd
sixiaoyou <yunshaliying@sina.com>
2017-12-21 07:15:15 +0800

    Init

---
+++ LeetCode-Python2/2017.12/LeetCode746-Min Cost Climbing Stairs.py
@@ -0,0 +1,30 @@
+'''
+【Python3代码不变故可参考此Python2代码】
+【网友实现】:http://bookshadow.com/weblog/2017/12/17/leetcode-min-cost-climbing-stairs/
+On a staircase, the i-th step has some non-negative cost cost[i] assigned (0 indexed).
+
+Once you pay the cost, you can either climb one or two steps. You need to find minimum cost to reach the top of the floor, and you can either start from the step with index 0, or the step with index 1.
+
+Example 1:
+Input: cost = [10, 15, 20]
+Output: 15
+Explanation: Cheapest is start on cost[1], pay that cost and go to the top.
+Example 2:
+Input: cost = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1]
+Output: 6
+Explanation: Cheapest is start on cost[0], and only step on 1s, skipping cost[3].
+Note:
+cost will have a length in the range [2, 1000].
+Every cost[i] will be an integer in the range [0, 999].
+'''
+class Solution:
+    def minCostClimbingStairs(self, cost):
+        """
+        :type cost: List[int]
+        :rtype: int
+        """
+        size = len(cost)
+        dp = [cost[0],cost[1]]
+        for i in range(2,size):
+            dp.append(min(dp[i-1],dp[i-2])+cost[i])
+        return min(dp[-1],dp[-2])
