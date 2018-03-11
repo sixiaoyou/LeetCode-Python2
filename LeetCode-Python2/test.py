4c59131b615fa3976b459fa2a28fa9d491f89e7a
sixiaoyou <yunshaliying@sina.com>
2018-03-11 16:26:02 +0800

    Init

--- LeetCode-Python2/test.py
+++ LeetCode-Python2/test.py
@@ -1,15 +1,9 @@
-S="kqep"
-T="pekeq"
-# S="hucw"
-# T="utzoampdgkalexslxoqfkdjoczajxtuhqyxvlfatmptqdsochtdzgypsfkgqwbgqbcamdqnqztaqhqanirikahtmalzqjjxtqfnh"
-ls = list(S)
-dict = {}
-for i in T:
-    if i in ls:
-        if i not in dict:
-            index = ls.index(i)
-            ls = ls[:index]+T.count(i)*[i]+ls[index+1:]
-            dict[i] = i
-    else:
-        ls.append(i)
-print  "".join(ls)
+
+A="abcde"
+B="cdeab"
+for i in range(len(A)):
+    s = A[i + 1:] + A[:i+1]
+    print s
+    if s== B:
+        print True
+print False
