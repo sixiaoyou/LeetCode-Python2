22ad108c88efe8f0dab421521ca08c1ed1baa313
sixiaoyou <yunshaliying@sina.com>
2018-03-26 19:30:03 +0800

    Init

---
+++ LeetCode-Python2/2018.3/LeetCode806-Number of Lines To Write String.py
@@ -0,0 +1,60 @@
+'''
+【网友实现】
+【Python3代码不变，故可参照此Python2代码】
+We are to write the letters of a given string S, from left to right into lines. Each line has maximum width 100 units, and if writing a letter would cause the width of the line to exceed 100 units, it is written on the next line. We are given an array widths, an array where widths[0] is the width of 'a', widths[1] is the width of 'b', ..., and widths[25] is the width of 'z'.
+
+Now answer two questions: how many lines have at least one character from S, and what is the width used by the last such line? Return your answer as an integer list of length 2.
+
+
+
+Example :
+Input:
+widths = [10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10]
+S = "abcdefghijklmnopqrstuvwxyz"
+Output: [3, 60]
+Explanation:
+All letters have the same length of 10. To write all 26 letters,
+we need two full lines and one line with 60 units.
+Example :
+Input:
+widths = [4,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10]
+S = "bbbcccdddaaa"
+Output: [2, 4]
+Explanation:
+All letters except 'a' have the same length of 10, and
+"bbbcccdddaa" will cover 9 * 10 + 2 * 4 = 98 units.
+For the last 'a', it is written on the second line because
+there is only 2 units left in the first line.
+So the answer is 2 lines, plus 4 units in the second line.
+
+
+Note:
+
+The length of S will be in the range [1, 1000].
+S will only contain lowercase letters.
+widths is an array of length 26.
+widths[i] will be in the range of [2, 10].
+
+'''
+class Solution(object):
+    def numberOfLines(self, widths, S):
+        """
+        :type widths: List[int]
+        :type S: str
+        :rtype: List[int]
+        """
+        dict = {}
+        total = 0
+        lines=1
+        lastLine = 0
+        a=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+        for i in range(len(a)):
+            dict[a[i]] = widths[i]
+        for j in S:
+            if total+dict[j]<=100:
+                total+=dict[j]
+            else:
+                lines+=1
+                total=dict[j]
+            lastLine = total
+        return [lines,lastLine]
