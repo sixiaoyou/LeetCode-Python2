d1dc9ae21f19854cbd0388592bf01b28ee32ed9a
sixiaoyou <yunshaliying@sina.com>
2018-07-08 19:25:50 +0800

    Init

--- LeetCode-Python2/test.py
+++ LeetCode-Python2/test.py
@@ -1,20 +1,9 @@
-class Solution(object):
-    def flipAndInvertImage(self, A):
-        """
-        :type A: List[List[int]]
-        :rtype: List[List[int]]
-        """
-        for i in A:
-            i.reverse()
-        for i in range(len(A)):
-            for j in range(len(A[i])):
-                print A
-                if A[i][j] == 0:
-                    A[i][j] = 1
-                else:
-                    A[i][j] = 0
-        return A
-
-A=[[1,1,0],[1,0,1],[0,0,0]]
-a=Solution()
-print a.flipAndInvertImage(A)
+a=[[1,2,3],[4,5,6],[7,8,9]]
+b=len(a[0])
+c = len(a)
+d =[[0 for i in range(c)] for j in range(b)]
+print d
+for i in range(b):
+    for j in range(c):
+        d[i][j] = a[j][i]
+print d
